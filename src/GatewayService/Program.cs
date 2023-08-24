64f615ba41ced0c3002b54352b8f3b4255654209
lak <caizh1997@live.com>
2023-08-23 19:09:00 -0500

    add gateway service

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.Authority = builder.Configuration["IdentityServiceUrl"];
+        o.RequireHttpsMetadata = false;
+        o.TokenValidationParameters.ValidateAudience = false;
+        o.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
