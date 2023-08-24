70555ba229e44765a5dcc33e073118b9bc782641
lak <caizh1997@live.com>
2023-08-23 20:11:14 -0500

    add client for nextapp

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -28,6 +28,18 @@ public static class Config
                 RedirectUris = { "https://www.getpostman/oath2/callback" },
                 ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
                 AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
+            },
+            new()
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                AccessTokenLifetime = 3600 * 24 * 30
             }
         };
 }
