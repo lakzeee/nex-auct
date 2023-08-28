2b3601a7b1443fcb674dc50f7365e5cbdd5b8b67
lak <caizh1997@live.com>
2023-08-27 22:58:53 -0500

    add notification service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -15,8 +15,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         o.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy",
+        b =>
+        {
+            b
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins(builder.Configuration["ClientApp"]);
+        });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 app.MapReverseProxy();
 
 app.UseAuthentication();
