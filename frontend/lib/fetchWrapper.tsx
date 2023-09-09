23afbb8f8f304a7ff13dc7ff58c41e30ca173c47
lak <caizh1997@live.com>
2023-09-09 15:51:58 -0500

    add missing id-svc url to k8s file

--- frontend/lib/fetchWrapper.tsx
+++ frontend/lib/fetchWrapper.tsx
@@ -64,7 +64,7 @@ async function handleResponse(response: Response) {
   } else {
     const error = {
       status: response.status,
-      message: typeof data === "string" ? data : response.statusText,
+      message: typeof data === "string" && data.length > 0 ? data : response.statusText,
     };
     return { error };
   }
