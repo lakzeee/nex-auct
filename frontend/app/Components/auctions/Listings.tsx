6d75ea0c0648cbe2d5d008be3a047745d8e3e1b0
lak <caizh1997@live.com>
2023-09-10 22:47:33 -0500

    remove k8s cicd

--- frontend/app/Components/auctions/Listings.tsx
+++ frontend/app/Components/auctions/Listings.tsx
@@ -57,7 +57,8 @@ export default function Listings() {
       ) : (
         <>
           <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:gap-6">
-            {data.auctions.length > 0 &&
+            {data.auctions &&
+              data.auctions.length > 0 &&
               data.auctions.map((auction) => (
                 <AuctionCard auction={auction} key={auction.id} />
               ))}
